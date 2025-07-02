06fefc45db626e11b60640c20d57776d7101613e
GuhFullstack <gustaavvoo97@gmail.com>
2025-07-02 18:22:26 -0300

    lição

---
+++ OPERADORES/js/script.js
@@ -0,0 +1,52 @@
+// 1 - Number
+console.log(2);
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+//2 - Ops.aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+
+console.log(5 + 4 * 2);
+
+//3 - Special numbers
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+
+console.log(typeof NaN);
+
+//4 - STRINGS 
+console.log("Um texto");
+console.log(`Mais um texto`);
+
+//5 - Simbolos especiais em string
+console.log("Testando a \n quebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+//6 - Concactenação
+console.log("Oi, " + "tudo" + "bem?");
+console.log(`testando ` + `Com ` + `crase!`); 
+
+// 7 - Template Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - Boolean
+console.log(true);
+console.log(false);
+console.log(5 > 20);
+console.log(30 > 10);
+
+// 9 - Comparações
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 == 9);
