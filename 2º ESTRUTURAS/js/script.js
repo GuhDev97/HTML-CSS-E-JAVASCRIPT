141b782c3ab7a2b243df4e57fb926c3a6eefc62b
GuhFullstack <gustaavvoo97@gmail.com>
2025-07-22 23:10:16 -0300

    add

---
+++ 2º ESTRUTURAS/js/script.js
@@ -0,0 +1,39 @@
+// 1 - Variáveis
+let nome = "Matheus";
+
+console.log(nome);
+
+nome = "Gustavo";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32;
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - mais sobre variáveis
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10, b = 20, c = 30
+
+console.log(a, b, c);
+
+const nomecompleto = "Gustavo Dias da Silva"
+
+const nomeCompleto = "João"
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+let _teste = "ok"
+
+let $teste = "ok"
+
+console.log(_teste, $teste);
