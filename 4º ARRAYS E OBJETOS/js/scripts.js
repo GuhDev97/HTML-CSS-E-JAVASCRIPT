201b520d2f7ce762c780092775763959f93855db
GuhFullstack <gustaavvoo97@gmail.com>
2025-09-09 17:55:04 -0300

    atualização das aulas

--- 4º ARRAYS E OBJETOS/js/scripts.js
+++ 4º ARRAYS E OBJETOS/js/scripts.js
@@ -105,3 +105,125 @@ delete b.age;
 console.log(a);
 console.log(b);
 
+// 10 - loop em array 
+
+const users = ["Gustavo", "João", "Pedro", "Miguel"];
+
+for (let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`);
+}
+
+// 11 - push e pop
+
+const array = ["a", "b", "c"];
+
+array.push("d");
+
+console.log(array);
+
+console.log(array.length);
+
+array.pop();
+
+console.log(array);
+
+const itemRemovido = array.pop();
+
+console.log(itemRemovido);
+
+console.log(array);
+
+array.push("z", "x", "y");
+
+console.log(array);
+
+// 12 - shift e unshift
+
+const letters = ["a", "b", "c"];
+
+const letter = letters.shift();
+
+console.log(letter);
+
+console.log(letters);
+
+letters.unshift("p", "q", "r");
+
+letters.unshift("z");
+
+console.log(letters);
+
+// 13 - indexOf e lastindexOf
+
+const myElements = ["Morango", "Maça", "Abacate", "Pêra", "Abacate"];
+
+console.log(myElements.indexOf("Maça"));
+console.log(myElements.indexOf("Abacate"));
+
+console.log(myElements[2]);
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Abacate"));
+
+console.log(myElements.indexOf("Mamão"));
+console.log(myElements.lastIndexOf("Mamão"));
+
+
+// 14 - slice
+
+ const testeSlice = ["a", "b", "c", "d", "e", "f"];
+
+ const subArray = testeSlice.slice(2, 4);
+
+ console.log(subArray);
+
+ console.log(testeSlice);
+
+ const subArray2 = testeSlice.slice(2, 4 + 1);
+ console.log(subArray2);
+
+ const subArray3 = testeSlice.slice(10, 20);
+
+ console.log(subArray3);
+
+ const subArray4 = testeSlice.slice(2);
+
+ console.log(subArray4);
+
+ // 15 - foreach
+ const nums = [1, 2, 3, 4, 5];
+
+ nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+ });
+
+ const posts = [
+    { title: "Primeiro post", category: "PHP" },
+    { title: "Segundo post", category: "JavaScript" },
+    { title: "Terceiro post", category: "Python" },
+ ];
+
+ posts.forEach((post) =>{
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`);
+ });
+
+ // 16 - includes
+const brands = ["BMW", "VW", "Fiat"];
+
+console.log(brands.includes("Fiat"));
+
+console.log(brands.includes("KIA"));
+    
+if (brands.includes("BMW")) {
+    console.log("Há carros da marca BMW!");
+}
+
+// 17 - reverse
+
+const reverseTest = [1, 2, 3, 4, 5];
+
+reverseTest.reverse();
+
+console.log(reverseTest);
+
+
