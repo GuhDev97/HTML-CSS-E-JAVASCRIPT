e9495c3cf0754482b815919493e32d59a9fa3ddc
GuhFullstack <gustaavvoo97@gmail.com>
2025-09-30 13:35:16 -0300

    att

--- 5º OOP/js/script.js
+++ 5º OOP/js/script.js
@@ -9,3 +9,40 @@ const animal = {
 console.log(animal.nome);
 
 animal.latir();
+
+// 2 - aprofundando em métodos
+const pessoa = {
+    nome: "Matheus", 
+
+    getNome: function () {
+        return this.nome;
+    },
+
+    setNome: function (novoNome) {
+        this.nome = novoNome;
+    },
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.getNome());
+
+pessoa.setNome("joaquim");
+
+console.log(pessoa.getNome());
+
+// 3 - prototype
+
+const text = "asd";
+
+console.log(Object.getPrototypeOf(text));
+
+const bool = true;
+
+console.log(Object.getPrototypeOf(bool));
+
+const arr = [];
+
+console.log(arr.length);
+
+console.log(Object.getPrototypeOf(arr));
