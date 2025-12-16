41e837a37879855fa4a1d2c270e86d81433a9d8a
GuhFullstack <gustaavvoo97@gmail.com>
2025-12-16 20:17:59 -0300

    projeto

---
+++ 7º ASYNC/js/script.js
@@ -0,0 +1,36 @@
+// 1 - setTimeout
+console.log("Ainda não executou")
+
+setTimeout(function(){
+    console.log ("Requisição assíncrona");
+}, 2000);
+
+console.log("Ainda não executou 2")
+
+// 2 - setInterval
+
+console.log("Ainda não começou")
+
+// setInterval(function(){
+//     console.log ("Requisição assíncrona");
+// }, 3000);
+
+console.log("Ainda não começou 2");
+
+//3 - Promise
+
+const promessa = Promise.resolve(5+5);
+
+console.log("Algum código");
+
+promessa
+    .then((value) => {
+        console.log(`A soma é ${value}`);
+        return value;
+
+    })
+
+.then((value) => value - 1)
+.then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código");
