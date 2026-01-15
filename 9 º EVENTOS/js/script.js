72fd0f821dded1a5789be8f4922c722b0222cdf7
GuhFullstack <gustaavvoo97@gmail.com>
2026-01-14 23:54:19 -0300

    att das aulas

--- 9 º EVENTOS/js/script.js
+++ 9 º EVENTOS/js/script.js
@@ -6,3 +6,46 @@ btn.addEventListener("Click", function()
 {
     console.log("Clicou aqui");
 });
+
+
+// 2 - removendo evento
+
+const secondBtn = document.querySelector("#btn");
+
+function imprimirMensagem() {
+    console.log("teste");
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
+
+// 3 - argumento do evento
+
+const myTitle = document.querySelector("#my-title");
+
+myTitle.addEventListener("click", (event) => {
+    console.log(event);
+    console.log(event.offsetx);
+    console.log(event.pointerType);
+    console.log(event.target);
+});
+
+// 4 - propagação 
+
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2");
+});
